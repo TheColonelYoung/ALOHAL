f7a3ec7342132fa28c4d36b13ffc646a9dc8db4c
TheColonelYoung <Malanik.Petr.gm@gmail.com>
2018-02-21 12:31:56 +0100

    Update ADC ALOHAL library. Add class oriented approach. ADC class is now affected by global access.
    Signed-off-by: Petr Malanik <Malanik.Petr.gm@gmail.com>

--- adc/ADC.hpp
+++ adc/ADC.hpp
@@ -3,6 +3,7 @@
 
 #ifdef STM32_F0
 # include "stm32f0xx_hal.h"
+# include "stm32f0xx_hal_adc.h"
 #elif STM32_F1
 # include "stm32f1xx_hal.h"
 #elif STM32_F3
@@ -11,15 +12,59 @@
 # include "stm32f4xx_hal.h"
 #elif STM32_F7
 # include "stm32f7xx_hal.h"
-#endif
+#endif // ifdef STM32_F0
+
+using namespace std;
+
+#include <cstdint>
+#include <string>
+#include <memory>
 
 #include "gpio/pin.hpp"
-#include "usb/USB_VCP.hpp"
+#include "globals.hpp"
 
-#define ADC_REF_VOLTAGE 	2.89			//voltage of external reference
-#define VREFINT_CAL_ADDR    0x1FFFF7BA
+#define ADC_REF_VOLTAGE  2.89 // voltage of external reference
+#define VREFINT_CAL_ADDR ((uint16_t *)((uint32_t) 0x1FFFF7BA))
 
-//uint16_t VREFINT_CAL = *((uint16_t*)VREFINT_CAL_ADDR); // VREFINT calibration value
+#define ADC_CHANNEL_CONCAT(channel) (ADC_CHANNEL_ ## channel)
+
+// uint16_t VREFINT_CAL = *((uint16_t*)VREFINT_CAL_ADDR); // VREFINT calibration value
+// VDD = 3.3 * (VREFINT_CAL / VREFINT_DATA)
+
+class AD_C {
+    ADC_HandleTypeDef *handler;
+
+    int actual_channel = -1;
+
+    uint16_t reference_val; // Actual reference value
+    uint16_t reference_cal; // Reference value for 3.3V
+    uint16_t ADC_val;       // Value measured by ADC
+
+    uint supply_voltage = 3300; // in milivolts
+    uint resolution = 12;
 
 
-#endif
+public:
+    AD_C() = default;
+    AD_C(ADC_HandleTypeDef *handler);
+
+    int Set_channel(int chan);          // Set actual channel, allowed values: 0-15
+    int Set_channel(string chan);       // Allowed values: VREF(16), TEMP(17), VBAT(18)
+    int Set_resolution(int resolution);
+
+    void Set_value(uint16_t value); // Set measured value
+
+    uint16_t Measure(); // Rin one pulse measuring ! polling => blocking
+    void Start_IT();    // Start one measuring with interrupt
+
+    uint Calibration(); // Measure supply voltage and save as reference
+
+    uint Supply_voltage(); // Supply voltage in miliVolts;
+    uint16_t Value();      // Measured value in register
+
+    float Voltage_v(); // Measured voltage in Volts
+    uint Voltage();    // In miliVolts
+    uint Voltage_u();  // In microVolts
+};
+
+#endif // ifndef ADC_H
