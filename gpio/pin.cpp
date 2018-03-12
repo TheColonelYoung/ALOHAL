5dbbdcdedcd25528d5c64368e125108b222a1f08
petr <Malanik.Petr.gm@gmail.com>
2018-03-12 15:23:30 +0100

    Add read method for Pin.

--- gpio/pin.cpp
+++ gpio/pin.cpp
@@ -1,28 +1,27 @@
 #include "pin.hpp"
 
-Pin::Pin(char port_set, int pin_number_set)
-{
-	port       = port_set;
-	pin_number = pin_number_set;
+Pin::Pin(char port_set, int pin_number_set){
+    port       = port_set;
+    pin_number = pin_number_set;
 }
 
-void Pin::Toggle()
-{
-	HAL_GPIO_TogglePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin_number);
+void Pin::Toggle(){
+    HAL_GPIO_TogglePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin_number);
 }
 
-void Pin::Set(bool value)
-{
-	HAL_GPIO_WritePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin_number, (GPIO_PinState) value);
+void Pin::Set(bool value){
+    HAL_GPIO_WritePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin_number, (GPIO_PinState) value);
+}
+
+bool Pin::Read(){
+    return HAL_GPIO_ReadPin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin_number);
 }
 
 // Base port adress:1207959552
-void Pin_set(char port, int pin, bool value)
-{
-	HAL_GPIO_WritePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin, (GPIO_PinState) value);
+void Pin_set(char port, int pin, bool value){
+    HAL_GPIO_WritePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin, (GPIO_PinState) value);
 } /* Pin_set */
 
-void Pin_tog(char port, int pin)
-{
-	HAL_GPIO_TogglePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin);
+void Pin_tog(char port, int pin){
+    HAL_GPIO_TogglePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin);
 }
