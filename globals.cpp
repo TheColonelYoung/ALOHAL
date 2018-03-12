43b17c2a87fd9c79d68791b786134d4a47b78ff4
TheColonelYoung <Malanik.Petr.gm@gmail.com>
2018-03-12 18:22:04 +0100

    Add timer to global configuration

--- globals.cpp
+++ globals.cpp
@@ -1,13 +1,12 @@
 #include "globals.hpp"
 
 void ALOHAL_init(){
-
-    //ADC
+    // ADC
     #ifdef ADC_1_EN
     ADC_1 = AD_C(&hadc);
     #endif
 
-    //UART
+    // UART
     #ifdef UART_1_EN
     UART1 = UART(&huart1);
     #endif
@@ -21,14 +20,38 @@ void ALOHAL_init(){
     UART4 = UART(&huart4);
     #endif
 
-}
+    #ifdef TIM_1_EN
+    TIM_1 = Timer(&htim1,16,4);
+    #endif
+    #ifdef TIM_2_EN
+    TIM_2 = Timer(&htim2);
+    #endif
+    #ifdef TIM_3_EN
+    TIM_3 = Timer(&htim3);
+    #endif
+    #ifdef TIM_4_EN
+    TIM_4 = Timer(&htim4);
+    #endif
+    #ifdef TIM_5_EN
+    TIM_5 = Timer(&htim5);
+    #endif
+    #ifdef TIM_6_EN
+    TIM_6 = Timer(&htim6);
+    #endif
+    #ifdef TIM_7_EN
+    TIM_7 = Timer(&htim7);
+    #endif
+    #ifdef TIM_8_EN
+    TIM_8 = Timer(&htim8);
+    #endif
+} // ALOHAL_init
 
-//ADC
+// ADC
 #ifdef ADC_1_EN
 AD_C ADC_1;
 #endif
 
-//UART
+// UART
 #ifdef UART_1_EN
 UART UART1;
 #endif
@@ -40,4 +63,29 @@ UART UART3;
 #endif
 #ifdef UART_4_EN
 UART UART4;
+#endif
+
+#ifdef TIM_1_EN
+Timer TIM_1;
+#endif
+#ifdef TIM_2_EN
+Timer TIM_2;
+#endif
+#ifdef TIM_3_EN
+Timer TIM_3;
+#endif
+#ifdef TIM_4_EN
+Timer TIM_4;
+#endif
+#ifdef TIM_5_EN
+Timer TIM_5;
+#endif
+#ifdef TIM_6_EN
+Timer TIM_6;
+#endif
+#ifdef TIM_7_EN
+Timer TIM_7;
+#endif
+#ifdef TIM_8_EN
+Timer TIM_8;
 #endif
--- globals.hpp
+++ globals.hpp
@@ -1,56 +1,121 @@
 #ifndef GLOBALS_H
-#define GLOBALS_H
+# define GLOBALS_H
 
 typedef unsigned int uint;
 
-#include <cstdint>
+# include <cstdint>
 
 void ALOHAL_init();
 
 // SETTINGS
 
-#define ADC_1_EN
-//NOTE In higher families MCUs has more ADC, in this purpose conditional compile must be added
+// #define ADC_1_EN
+// NOTE In higher families MCUs has more ADC, in this purpose conditional compile must be added
 
-#define UART_1_EN
+// #define UART_1_EN
 // #define UART_2_EN
 // #define UART_3_EN
 // #define UART_4_EN
 
-//ADC
+# define TIM_1_EN
+# define TIM_2_EN
+// #define TIM_3_EN
+// #define TIM_4_EN
+// #define TIM_5_EN
+// #define TIM_6_EN
+// #define TIM_7_EN
+// #define TIM_8_EN
+
+// ADC
 class AD_C;
 
-#ifdef ADC_1_EN
-#include "adc/ADC.hpp"
+# ifdef ADC_1_EN
+#  include "adc/ADC.hpp"
 extern ADC_HandleTypeDef hadc;
 extern AD_C ADC_1;
-#endif
+# endif
 
-//UART
+// UART
 class UART;
 
-#ifdef UART_1_EN
-#include "uart/uart.hpp"
+# ifdef UART_1_EN
+#  include "uart/uart.hpp"
 extern UART_HandleTypeDef huart1;
 extern UART UART1;
-#endif
+# endif
 
-#ifdef UART_2_EN
-#include "uart/uart.hpp"
+# ifdef UART_2_EN
+#  include "uart/uart.hpp"
 extern UART_HandleTypeDef huart2;
 extern UART UART2;
-#endif
+# endif
 
-#ifdef UART_3_EN
-#include "uart/uart.hpp"
+# ifdef UART_3_EN
+#  include "uart/uart.hpp"
 extern UART_HandleTypeDef huart3;
 extern UART UART3;
-#endif
+# endif
 
-#ifdef UART_4_EN
-#include "uart/uart.hpp"
+# ifdef UART_4_EN
+#  include "uart/uart.hpp"
 extern UART_HandleTypeDef huart4;
 extern UART UART4;
+# endif
+
+#endif // ifndef GLOBALS_H
+
+class Timer;
+
+#ifdef TIM_1_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim1;
+extern Timer TIM_1;
 #endif
 
+#ifdef TIM_2_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim2;
+extern Timer TIM_2;
+#endif
+
+#ifdef TIM_3_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim3;
+extern Timer TIM_3;
+#endif
+
+#ifdef TIM_4_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim4;
+extern Timer TIM_4;
+#endif
+
+#ifdef TIM_5_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim5;
+extern Timer TIM_5;
+#endif
+
+#ifdef TIM_6_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim6;
+extern Timer TIM_6;
+#endif
+
+#ifdef TIM_7_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim7;
+extern Timer TIM_7;
+#endif
+
+#ifdef TIM_8_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim8;
+extern Timer TIM_8;
+#endif
+
+#ifdef TIM_1_EN
+# include "timer/timer.hpp"
+extern TIM_HandleTypeDef htim1;
+extern Timer TIM_1;
 #endif
