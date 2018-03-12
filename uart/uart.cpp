bfc66363909770a8058f693804566bc9fb24a02c
TheColonelYoung <Malanik.Petr.gm@gmail.com>
2018-03-13 00:42:30 +0100

    Add asynchronous UART
    Now are messages saved in queve and wait for unoccupied UART

--- uart/uart.cpp
+++ uart/uart.cpp
@@ -36,13 +36,19 @@ UART::UART(UART_HandleTypeDef *UART_Handler_set){
 }
 
 int UART::Send(string message){
-    if (HAL_UART_Transmit_IT(UART_Handler, (unsigned char *) message.c_str(), message.length()) != HAL_OK) { }
+    if (busy) {
+        TX_buffer.push_back(message);
+        return TX_buffer.size();
+    }
+    busy = true;
+    TX_buffer.push_back(message);
+    HAL_UART_Transmit_IT(UART_Handler, (unsigned char *) TX_buffer.front().c_str(), TX_buffer.front().length());
     return message.length();
 }
 
 int UART::Send(int number){
     string message = to_string(number);
-    HAL_UART_Transmit_IT(UART_Handler, (unsigned char *) message.c_str(), message.length());
+    Send(message);
     return message.length();
 }
 
@@ -50,58 +56,74 @@ int UART::Send(uint number){
     return Send((int) number);
 }
 
-int UART::Load(int count){
+int UART::Load(){
     UART_buffer.push_back(UART_buffer_temp[0]);
     HAL_UART_Receive_IT(UART_Handler, UART_buffer_temp, 1);
-    return count;
+    return 0;
 }
 
 int UART::Clear_buffer(){
+    uint length = UART_buffer.length();
     UART_buffer = "";
-    return UART_buffer.length();
+    return length;
 }
 
-void UART1_IT_Handler(){
-    UART1.Send(UART1.UART_buffer);
-    UART1.Clear_buffer();
+int UART::Resend(){
+    TX_buffer.erase(TX_buffer.begin()); //Erase message which transfer is complete
+    if (TX_buffer.size() > 0) { //Send next message in line
+        HAL_UART_Transmit_IT(UART_Handler, (unsigned char *) TX_buffer.front().c_str(), TX_buffer.front().length());
+    }else{  //Now is UART unoccupied
+        busy = false;
+    }
+    return TX_buffer.size();
 }
-
-void UART2_IT_Handler(){ }
-
-void UART3_IT_Handler(){
-    // UART_1.Send(UART_3.UART_buffer.length());
-    // ESP_Receive();
-}
-
-void UART4_IT_Handler(){ }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
     #ifdef UART_1_EN
     if (huart->Instance == USART1) {
-        UART1.Load(1);
-        UART1_IT_Handler();
+        UART1.Load();
+        // UART1.IRQ.Notify();
     }
     #endif
     #ifdef UART_2_EN
     if (huart->Instance == USART2) {
-        UART2.Load(1);
+        UART2.Load();
         UART2_IT_Handler();
     }
     #endif
     #ifdef UART_3_EN
     if (huart->Instance == USART3) {
-        UART3.Load(1);
+        UART3.Load();
         UART3_IT_Handler();
     }
     #endif
     #ifdef UART_4_EN
     if (huart->Instance == USART4) {
-        UART4.Load(1);
+        UART4.Load();
         UART4_IT_Handler();
     }
     #endif
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
-    // Pin('C',14).Toggle();
+    #ifdef UART_1_EN
+    if (huart->Instance == USART1) {
+        UART1.Resend();
+    }
+    #endif
+    #ifdef UART_2_EN
+    if (huart->Instance == USART2) {
+        UART2.Resend();
+    }
+    #endif
+    #ifdef UART_3_EN
+    if (huart->Instance == USART3) {
+        UART3.Resend();
+    }
+    #endif
+    #ifdef UART_4_EN
+    if (huart->Instance == USART4) {
+        UART4.Resend();
+    }
+    #endif
 }
--- uart/uart.hpp
+++ uart/uart.hpp
@@ -20,9 +20,12 @@
 # include "stm32f7xx_hal_uart.h"
 #endif // ifdef STM32_F0
 
+#include <vector>
+#include "globals.hpp"
 #include "misc/text_transform.hpp"
 #include "gpio/pin.hpp"
-#include "globals.hpp"
+//#include "irq/irq_handler.hpp"
+
 
 using namespace std;
 
@@ -31,29 +34,42 @@ void UART2_IT_Handler();
 void UART3_IT_Handler();
 void UART4_IT_Handler();
 
+//class IRQ_handler;
+
 class UART {
 private:
+
+
     UART_HandleTypeDef *UART_Handler;
 
     unsigned char *UART_buffer_temp;
+
+    unsigned char *RX_buffer;
+    vector<string> TX_buffer;
+
     int buffer_end = 0;
 
 public:
+    //IRQ_handler IRQ;
     string UART_buffer = "";
+    bool busy = false;
 
     UART(UART_HandleTypeDef *UART_Handler_set);
     UART(){ };
+
     int Send(string message); // send strinh over UART
     int Send(int message);    // send number over UART
     int Send(uint message);    // send number over UART
 
-    int Load(int count); // read specified number of chars from HW buffer to UART_buffer
+    int Resend();
+
+    int Load();
     int Receive();       // Receive IT Callback -> Insert into
 
     int Clear_buffer();  // Clear input buffer
 };
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
-
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
 
 #endif // ifndef UART_H
