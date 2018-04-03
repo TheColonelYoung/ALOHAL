15e7dd222f4cdf199281ff50c85352763c49eef7
TheColonelYoung <Malanik.Petr.gm@gmail.com>
2018-04-03 21:40:38 +0200

    Rewrite DRV8825 stepper motor driver Now is functionality mostly based on parent class stepper motor. Implement Step method. Resolve doubling of steps.

--- motor/SM_DRV8825.cpp
+++ motor/SM_DRV8825.cpp
@@ -1,72 +1,79 @@
 #include "SM_DRV8825.hpp"
 
-// globals
-vector<SM_DRV8825> SM_DRV8825_stash;
+SM_DRV8825::SM_DRV8825(float step_size, float speed)
+    : StepperMotor(step_size, speed * 2){
+};
 
-SM_DRV8825::SM_DRV8825(int speed_set, bool direction_set, int steps_set, Pin step_pin_set, Pin direction_pin_set, Pin reset_pin_set, Pin sleep_pin_set, Pin fault_pin_set)
-{
-	_id       = SM_Get_freeID();
-	speed     = speed_set;
-	direction = direction_set;
-	steps     = steps_set;
-
-	_step_pin      = step_pin_set;
-	_direction_pin = direction_pin_set;
-
-	_reset_pin = reset_pin_set;
-	_sleep_pin = sleep_pin_set;
-	_fault_pin = fault_pin_set;
-
-	_reset_pin.Set(1);
-    _direction_pin.Set(direction_set);
-
-	if (steps == 0) {
-		Sleep();
-	} else  {
-		Wake_up();
-	}
+SM_DRV8825::SM_DRV8825(float step_size, float speed, float acceleration)
+    : StepperMotor(step_size, speed * 2, acceleration){
+};
 
 
-	SM_DRV8825_stash.push_back(*this);
+int SM_DRV8825::Set_pin(string pin_name, Pin pin_set){
+    if (pin_name == "step") {
+        _step_pin = pin_set;
+        return 1;
+    } else if (pin_name == "direction") {
+        _direction_pin == pin_set;
+        return 2;
+    } else if (pin_name == "reset") {
+        _reset_pin == pin_set;
+        _reset_pin.Set(1);
+        return 3;
+    } else if (pin_name == "sleep") {
+        _sleep_pin == pin_set;
+        return 4;
+    } else if (pin_name == "fault") {
+        _fault_pin == pin_set;
+        return 5;
+    }
+    return -1;
 }
 
-void SM_DRV8825::Step()
-{
-	_step_pin.Toggle();
+void SM_DRV8825::Select_timer(Timer &timer){
+    this->timer = timer;
+    timer.IRQ.Register(this, &SM_DRV8825::Step);
 }
 
-void SM_DRV8825::Sleep()
-{
-	_sleep_pin.Set(0);
+void SM_DRV8825::Step(){
+    if (steps != 0) {
+        _step_pin.Toggle();
+        // steps are limited
+        if (steps > 0) {
+            steps -= 1;
+            // steps decresed to 0 => set driver to sleep
+            if (steps <= 0) {
+                //Sleep();
+                Stop();
+            }
+        }
+    }
+    if (acc_en) {
+        Acc_step();
+    }
 }
 
-void SM_DRV8825::Wake_up()
-{
-	_sleep_pin.Set(1);
+long SM_DRV8825::Move(double degrees, SM_Directions direction){
+    StepperMotor::Move(degrees *2, direction);
+    return steps;
 }
 
-void SM_DRV8825::Set_direction(bool dir)
-{
-	if (dir != direction) {
-		direction = dir;
-		_direction_pin.Set(dir);
-	}
+int SM_DRV8825::Speed(float speed){
+    return StepperMotor::Speed(speed * 2);
 }
 
-void SM_DRV8825_Finder()
-{
-	for (auto &DRV : SM_DRV8825_stash) {
-		// id motor isnt stopped
-		if (DRV.steps != 0) {
-			DRV.Step();
-			// steps are limited
-			if (DRV.steps > 0) {
-				DRV.steps -= 1;
-				// steps decresed to 0 => set driver to sleep
-				if (DRV.steps == 0) {
-					DRV.Sleep();
-				}
-			}
-		}
-	}
+void SM_DRV8825::Sleep(){
+    _sleep_pin.Set(0);
+}
+
+void SM_DRV8825::Wake_up(){
+    _sleep_pin.Set(1);
+}
+
+void SM_DRV8825::Reset(){
+    _reset_pin.Set(0);
+}
+
+bool SM_DRV8825::Fault_check(){
+    return _fault_pin.Read();
 }
--- motor/SM_DRV8825.hpp
+++ motor/SM_DRV8825.hpp
@@ -2,25 +2,14 @@
  * Author:	TCY
  * Name:	Library for usage of SM driver DRV8825 for STM32
  * E-mail:	TheColonelYoung(at)gmail(dot)com
- * Date:	1.7.2017
+ * Date:	26.3.2018
  * Description:
- *
- * ????????????????????HELP????????????????????
- *
- * !!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!
- *
- * --------------------BUGS--------------------
- *++++++++++++++++++++++ TODO++++++++++++++++++++
- *
- *
- **********************************************/
+ */
+
+
 #ifndef SM_DRV8825_H
 #define SM_DRV8825_H
 
-// c++ libraries
-#include <string>
-
-// libraries needed for this library to run
 #ifdef STM32_F0
 # include "stm32f0xx_hal.h"
 # include "stm32f0xx_hal_uart.h"
@@ -38,46 +27,37 @@
 # include "stm32f7xx_hal_uart.h"
 #endif // ifdef STM32_F0
 
+#include <string>
 
-#include "motor/StepperMotor.hpp"
-#include "timer/tasker.hpp"
-#include "misc/text_transform.hpp"
-
-// libraries nedded for function of tasker events and DEBUG
-
-// defines
-
-// declaration
-class Tasker;
-class SM_DRV8825;
-
-// externs
-extern vector<SM_DRV8825> SM_DRV8825_stash;
-extern Tasker tasker;
+#include "globals.hpp"
+#include "motor/stepper_motor.hpp"
 
 using namespace std;
 
-// function prototypes
-void SM_DRV8825_Finder();
-
 // classes
 class SM_DRV8825 : public StepperMotor {
-	Pin _direction_pin;
-	Pin _step_pin;
-	Pin _reset_pin;
-	Pin _sleep_pin;
-	Pin _fault_pin;
+    Pin _direction_pin;
+    Pin _step_pin;
+    Pin _reset_pin;
+    Pin _sleep_pin;
+    Pin _fault_pin;
 
 public:
-	SM_DRV8825(int speed_set, bool direction_set, int steps_set, Pin step_pin_set, Pin direction_pin_set, Pin reset_pin_set, Pin sleep_pin_set, Pin fault_pin_set);
+    SM_DRV8825() = default;
+    SM_DRV8825(float step_size, float speed);
+    SM_DRV8825(float step_size, float speed, float acceleration);
 
+    int Set_pin(string pin_name, Pin pin_set);
+    void Select_timer(Timer &timer) override;
 
-	void Step();
-	void Reset();                 // reset driver
-	void Sleep();                 // set driver to sleep
-	void Wake_up();               // give up driver from sleep
-	bool Fault_check();           // check for fault on FAULT pin
-	void Set_direction(bool dir); // set direction, change pin and var value
+    long Move(double degrees, SM_Directions direction) override;
+    int Speed(float speed) override;
+    void Step() override;
+
+    void Reset();       // reset driver
+    void Sleep();       // set driver to sleep
+    void Wake_up();     // give up driver from sleep
+    bool Fault_check(); // check for fault on FAULT pin
 };
 
 #endif // ifndef SM_DRV8825_H
