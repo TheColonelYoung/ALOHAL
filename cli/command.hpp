451051d3452b369543d804b16f5b59a7a866a062
Petr Malanik <malanik.petr.gm@gmail.com>
2019-07-14 21:18:47 +0200

    cli: add class command which can hold command, it could be object and method or function

---
+++ cli/command.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Command_base
+{
+private:
+public:
+    Command_base() = default;
+    ~Command_base() = default;
+
+    virtual const int Invoke(vector<string> &args) = 0;
+    virtual const string Get_command() = 0;
+    virtual const string Get_help() = 0;
+};
+
+template<typename template_class>
+class Command : public Command_base{
+private:
+    string command;
+    string help;
+
+    template_class object;
+    template_class * object_ptr = nullptr;
+    int (template_class::*method_pointer)(vector<string> args);
+
+    int (*function)(vector<string> args) = nullptr;
+
+public:
+    Command() =default;
+    ~Command() =default;
+
+    Command(string command, string help, template_class &object, int (template_class::*method)(vector<string> args))
+        :command(command), help(help), object(object), method_pointer(method)
+    {}
+
+    Command(string command, string help, template_class *object, int (template_class::*method)(vector<string> args))
+        :command(command), help(help), object_ptr(object), method_pointer(method)
+    {}
+
+    Command(string command, string help,int (*function_pointer_set)(vector<string> args))
+        :command(command), help(help), function(function)
+    {}
+
+    const int Invoke(vector<string> &args) override {
+        if (function) {
+            return (*(function))(args);
+        } else if (object_ptr){
+            return (*object_ptr.*method_pointer)(args);
+        }else {
+            return (object.*method_pointer)(args);
+        }
+        return -1;
+    }
+
+    inline const string Get_command() override {return command;};
+    inline const string Get_help() override {return help;};
+};
+
+
+
