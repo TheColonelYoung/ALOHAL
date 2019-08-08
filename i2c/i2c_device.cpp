80fe4c1233175d9aee6d9e458106270f3693bccd
Petr Malanik <malanik.petr.gm@gmail.com>
2019-08-08 18:54:31 +0200

    i2c_device: class for devices connected to mcu via I2C bus

---
+++ i2c/i2c_device.cpp
@@ -0,0 +1,2 @@
+#include "i2c_device.hpp"
+
---
+++ i2c/i2c_device.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file i2c_device.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 08.08.2019
+ */
+
+#pragma once
+
+class I2C_device
+{
+private:
+public:
+    I2C_device() =default;
+};
+
+
