515f8b075cafd26c83b7c123b1df13ff08faee2f
Petr Malanik <malanik.petr.gm@gmail.com>
2019-08-09 21:49:19 +0200

    i2c_device: methods for transmitting and receiving to/from master on bus

--- i2c/i2c_device.cpp
+++ i2c/i2c_device.cpp
@@ -1,2 +1,17 @@
 #include "i2c_device.hpp"
 
+I2C_device::I2C_device(I2C_master master, uint8_t address):
+    master(master), address(address){
+
+}
+
+uint I2C_device::Transmit(vector<uint8_t> data){
+    return master.Transmit_poll(address, data);
+}
+
+vector<uint8_t> I2C_device::Receive(uint length){
+    return master.Receive_poll(address, length);
+}
+
+
+
--- i2c/i2c_device.hpp
+++ i2c/i2c_device.hpp
@@ -7,11 +7,45 @@
 
 #pragma once
 
+#include <vector>
+#include <string>
+
+#include "i2c/i2c_master.hpp"
+typedef unsigned int uint;
+
+/**
+ * @brief Generic class for all devices, which are connected to I2C bus
+ *
+ */
 class I2C_device
 {
 private:
+    I2C_master master;
+    uint8_t address = 0;
+
 public:
-    I2C_device() =default;
+    I2C_device() = default;
+    /**
+     * @brief Construct a new i2c device object
+     *
+     * @param master Object of master bus on which is device connected
+     * @param address Address of device on bus
+     */
+    I2C_device(I2C_master master, uint8_t address);
+
+    /**
+     * @brief Transmit data to device
+     *
+     * @param data Data to be send by bus
+     * @return uint Status code of transmittion
+     */
+    uint Transmit(vector<uint8_t> data);
+
+    /**
+     * @brief Receive data from device on bus
+     *
+     * @param length Number of bytes to be received
+     * @return vector<uint8_t> received data
+     */
+    vector<uint8_t> Receive(uint length);
 };
-
-
