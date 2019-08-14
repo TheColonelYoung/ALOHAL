75b2da95683d8e5cd638ae967257b2f1b8c0515d
Petr Malanik <malanik.petr.gm@gmail.com>
2019-08-15 00:01:45 +0200

    MCP23017: methods for selecting direction of pins

--- gpio/mcp23017/mcp23017.cpp
+++ gpio/mcp23017/mcp23017.cpp
@@ -12,3 +12,33 @@ int MCP23017::Init(){
     return ret;
 }
 
+int MCP23017::Direction(uint8_t pin, bool direction){
+    if((pin > 15) || (pin < 15)){
+        // Invalid pin number
+        return -1;
+    }
+
+    uint16_t old_direction = direction;
+
+    if(direction){
+        direction |= (1<<pin);
+    } else {
+        direction &= ~(1<<pin);
+    }
+
+    if(direction == old_direction){
+        // Same value is already set, no need for transfer
+        return -2
+    }
+
+    return Transmit(vector<uint8_t> {REG::IODIR,
+                                    static_cast<uint8_t>((direction & 0xff00)>>8 ),
+                                    static_cast<uint8_t>(direction & 0x00ff)});
+}
+
+int MCP23017::Direction(uint16_t port){
+    direction = port;
+    return Transmit(vector<uint8_t> {REG::IODIR,
+                                    static_cast<uint8_t>((direction & 0xff00)>>8 ),
+                                    static_cast<uint8_t>(direction & 0x00ff)});
+}
