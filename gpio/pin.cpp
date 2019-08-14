cd19149044f545256a197bf24c87133449155210
Petr Malanik <malanik.petr.gm@gmail.com>
2019-08-14 23:05:40 +0200

    pin: removed deprecated functions, add doxygen documentation

--- gpio/pin.cpp
+++ gpio/pin.cpp
@@ -1,10 +1,8 @@
 #include "pin.hpp"
 
-
-Pin::Pin(char port_set, int pin_number_set){
-    port           = port_set;
-    pin_number     = pin_number_set;
-    IRQ.pin_number = (1 << pin_number);
+Pin::Pin(char port, uint8_t pin_number):
+    port(port), pin_number(pin_number){
+    IRQ->pin_number = (1 << pin_number);
 }
 
 void Pin::Toggle(){
@@ -21,13 +19,4 @@ bool Pin::Read(){
 
 bool Pin::operator == (const Pin& rhs){
     return ((this->port == rhs.port) && (this->pin_number == rhs.pin_number));
-}
-
-// Base port adress:1207959552
-void Pin_set(char port, int pin, bool value){
-    HAL_GPIO_WritePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin, (GPIO_PinState) value);
-} /* Pin_set */
-
-void Pin_tog(char port, int pin){
-    HAL_GPIO_TogglePin((GPIO_TypeDef *) (PORT_START_ADRESS + ((((int) port) - ASCII_BASE) * PORT_SIZE )), (uint16_t) 1 << pin);
 }
--- gpio/pin.hpp
+++ gpio/pin.hpp
@@ -1,8 +1,4 @@
-#ifndef PIN_H
-#define PIN_H
-
-// ------------------LIBRARY_OPTIONS----------------
-// ------------------LIBRARY_OPTIONS----------------
+#pragma once
 
 #include "global_includes.hpp"
 #include "pin_irq.hpp"
@@ -11,31 +7,58 @@
 #define ASCII_BASE        (65)
 #define PORT_START_ADRESS ((int) GPIOA)
 
-#define pin_set_m(port_name, pin_number, value) HAL_GPIO_WritePin(GPIO ## port_name, GPIO_PIN_ ## pin_number, value ? GPIO_PIN_RESET : GPIO_PIN_SET);
-#define pin_tog_m(port_name, pin_number)        HAL_GPIO_TogglePin(GPIO ## port_name, GPIO_PIN_ ## pin_number);
-
-void Pin_set(char port, int pin, bool value);
-void Pin_tog(char port, int pin);
-
-
+/**
+ * @brief Represent GPIO of MCU
+ * Must be configured in CubeMX
+ */
 class Pin {
     char port;
-    int pin_number;
+
+protected:
+    uint8_t pin_number;
 
 public:
-    Pin_IRQ IRQ = Pin_IRQ();
+    /**
+     * @brief Handler external interupts, IRQ must be configured in CubeMX
+     */
+    Pin_IRQ *IRQ = new Pin_IRQ();
 
 public:
-    Pin(char port_set = 'X', int pin_number_set = 0);
+    /**
+     * @brief Construct a new Pin object
+     *
+     * @param port_set Port name, X represents that given pin is not assigned
+     *                      to port, for example is connected via expander
+     * @param pin_number_set Pin number
+     */
+    Pin(char port = 0, uint8_t pin_number = -1);
 
+    /**
+     * @brief
+     *
+     * @param rhs Pin for
+     * @return true Pins are equal
+     * @return false Pins are different
+     */
     bool operator==(const Pin& rhs);
 
-    void Toggle();
-    void Set(bool value);
+    /**
+     * @brief Change logic level of pin to opposite from actual
+     */
+    virtual void Toggle();
+
+    /**
+     * @brief Set pin to logic level
+     *
+     * @param value Logic level to set
+     */
+    virtual void Set(bool value);
+
+    /**
+     * @brief Read logic level from pin
+     *
+     * @return true  High logic level on input
+     * @return false Low logic level on input
+     */
     virtual bool Read();
 };
-
-
-
-
-#endif // ifndef PIN_H
