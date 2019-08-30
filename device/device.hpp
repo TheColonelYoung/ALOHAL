b896873e16e389769f60603cfc10f5093ed8812b
Petr Malanik <malanik.petr.gm@gmail.com>
2019-08-30 21:20:36 +0200

    device: add vector of components with declaration of basic method and doc

--- device/device.hpp
+++ device/device.hpp
@@ -10,9 +10,14 @@
 #define register      // Deprecated in C++11.
 #endif  // #if __cplusplus > 199711L
 
+#include <string>
+#include <vector>
+#include <memory>
+
 #include "mcu/mcu.hpp"
 #include "cli/cli.hpp"
 #include "gpio/pin.hpp"
+#include "device/component.hpp"
 
 using namespace std;
 
@@ -23,11 +28,40 @@ public:
     MCU *mcu = new MCU();
     CLI *cli = nullptr;
 
+    vector<shared_ptr<Component>> components;
+
     Device() = default;
 
+    /**
+     * @brief   Initialization of device
+     *          Runs initialization of mcu
+     */
+    void Init();
+
+    /**
+     * @brief Creates and initialize CLI for device
+     *
+     * @param connection Uart connection which will be used for CLI
+     * @return int
+     */
     int Enable_CLI(UART *connection);
 
-    void Init();
+    /**
+     * @brief Creates new enumerated name from generic component name
+     *
+     * @param original_name     Name of generic component, example: L6470
+     * @return string           New generated name with enumeration, example: L6470_1
+     */
+    string New_component_name(string original_name);
+
+    /**
+     * @brief Add component to vector of known components
+     *
+     * @param new_component Component to add
+     * @return uint         Number of components
+     */
+    uint Register_component(shared_ptr<Component> new_component);
+
 };
 
 
