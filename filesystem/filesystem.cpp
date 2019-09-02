2f3f712bf47ad80e75a36627059286aa55a947b8
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-02 19:26:13 +0200

    filesystem: add definition of filesystem commands and connection to cli

--- filesystem/filesystem.cpp
+++ filesystem/filesystem.cpp
@@ -1,1 +1,38 @@
 #include "filesystem.hpp"
+
+Filesystem::Filesystem(shared_ptr<CLI> cli):
+    cli(cli){
+
+}
+
+const int Filesystem::Command_ls(vector<string> args){
+    if (args.size() != 0){
+        return -1; //Invalid parameters
+    }
+    string name = args[0];
+    vector<string> path;
+
+    if (name[0] != '/'){ // relative path, add absolute prefix
+        name = actual_position->Path() + name;
+    }
+    path.emplace_back("/");
+
+    return 0;
+}
+
+const vector<string> Filesystem::Create_entry_path(string filename){
+    vector<string> path;
+    if (filename[0] != '/'){ // relative path, add absolute prefix
+        filename = actual_position->Path() + filename;
+    }
+    path.emplace_back("/");
+    //find(filename.begin(), filename.end(), "/");
+
+    return path;
+
+}
+
+const FS_entry Filesystem::Get_entry(vector<string> path){
+
+
+}
--- filesystem/filesystem.hpp
+++ filesystem/filesystem.hpp
@@ -1,15 +1,41 @@
 #pragma once
 
 #include "directory.hpp"
+#include "cli/cli.hpp"
 
 #include <string>
 #include <vector>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
 
 class Filesystem
 {
 private:
-    Directory root;
+
+
+    shared_ptr<Directory> root { new Directory("/")};
+
+    shared_ptr<Directory> actual_position;
+
+    shared_ptr<CLI> cli;
 public:
     Filesystem() =default;
-    ~Filesystem();
+    Filesystem(shared_ptr<CLI> cli);
+
+    const int Command_ls(vector<string> args);
+    int Command_cd(vector<string> args);
+    const int Command_cat(vector<string> args);
+    const int Command_pwd(vector<string> args);
+
+    int Make_directory(string name);
+    int Make_executable(string name);
+
+    const vector<string> Create_entry_path(string filename);
+    const FS_entry Get_entry(vector<string> path);
+
+    const bool Entry_exists(string filename);
+
+    int Delete(string name);
 };
