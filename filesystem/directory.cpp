e3a97245959f3f6e575d198aabe3ebafd6976eed
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-04 19:06:34 +0200

    FS: directory: methods for checking presence of file, finding file by name and doc

--- filesystem/directory.cpp
+++ filesystem/directory.cpp
@@ -4,10 +4,28 @@ Directory::Directory(){
     type = Type::Directory ;
 }
 
-Directory::Directory(string name){
+Directory::Directory(const string name){
     if (name.find("/") == string::npos){
         this->name = name;
     } else {
-        name = "exception";
+        this->name = "exception";
     }
+}
+
+FS_entry* Directory::Get_entry(const string entry_name) const{
+    for(auto entry:content){
+        if(entry->Name() == entry_name){
+            return entry;
+        }
+    }
+    return nullptr;
+}
+
+const bool Directory::Exists(const string filename){
+    for(auto entry:content){
+        if(entry->Name() == filename){
+            return true;
+        }
+    }
+    return false;
 }
--- filesystem/directory.hpp
+++ filesystem/directory.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file directory.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 05.09.2019
+ */
 #pragma once
 
 #include "entry.hpp"
@@ -11,16 +17,45 @@ class Directory: public FS_entry
 {
 private:
     string name = "None";
-protected:
     vector<FS_entry *> content;
+
 public:
     Directory();
-    Directory(string name);
+    Directory(const string name);
 
     int Delete_entry(FS_entry &entry);
     int Add_entry(FS_entry &entry);
 
-    const inline string Get_name(){return name;};
-    const inline int Contains(){return content.size();};
+    /**
+     * @brief   Count number of entries
+     *
+     * @return const int    Number of entries in Directory
+     */
+    const inline int Size(){return content.size();};
+
+    /**
+     * @brief Return entries, which are contained in Directory
+     *
+     * @return const vector<FS_entry *> Content of folder
+     */
+    const inline vector<FS_entry *> Contains(){return content;};
+
+    /**
+     * @brief Returns pointer to directory defined by filename
+     *
+     * @param entry_name        File name to search
+     * @return const FS_entry*  Pointer to entry with given name
+     */
+    FS_entry* Get_entry(const string entry_name) const;
+
+    /**
+     * @brief Check if entry with given name exists in folder
+     *
+     * @param filename  Filename to check
+     * @return true     File exists
+     * @return false    File not exists
+     */
+    const bool Exists(const string filename);
+
 };
 
