fa261bf49316bb0ead0ac2e0e4dcc95e5c8883a7
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-08 19:02:25 +0200

    filesystem: fixed middle filename issue when finding entry
    Entry was return even when was not last in entry filename.
    Example: /1/2/1, will return first folder named 1, not second

--- filesystem/filesystem.cpp
+++ filesystem/filesystem.cpp
@@ -1,6 +1,6 @@
 #include "filesystem.hpp"
 
-Filesystem::Filesystem(CLI* cli):
+Filesystem::Filesystem(CLI *cli) :
     cli(cli){
     cli->Register_command("ls", "ls as in Linux", this, &Filesystem::Command_ls);
     cli->Register_command("cd", "cd as in Linux", this, &Filesystem::Command_cd);
@@ -10,22 +10,22 @@ Filesystem::Filesystem(CLI* cli):
 
 int Filesystem::Command_ls(vector<string> args){
     string directory_name = Command_check(args);
-    if (directory_name == ""){
+    if (directory_name == "") {
         return -1;
     }
 
-    Directory* list_directory = static_cast<Directory*>(Get_entry(directory_name));
-    if(list_directory == nullptr){
+    Directory *list_directory = static_cast<Directory *>(Get_entry(directory_name));
+    if (list_directory == nullptr) {
         cli->Print("Target location is unreachable - null returned \r\n");
     }
 
-    if(list_directory->Type_of() != FS_entry::Type::Directory){
+    if (list_directory->Type_of() != FS_entry::Type::Directory) {
         cli->Print("Target location is not a directory \r\n");
         return -1;
     }
 
     string output = "";
-    for(auto entry:list_directory->Contains()){
+    for (auto entry:list_directory->Contains()) {
         output += entry->Name() + "\r\n";
     }
     cli->Print(output);
@@ -34,18 +34,18 @@ int Filesystem::Command_ls(vector<string> args){
 
 int Filesystem::Command_cd(vector<string> args){
     string directory_name = Command_check(args);
-    if (directory_name == ""){
+    if (directory_name == "") {
         return -1;
     }
 
     cli->Print(directory_name + "\r\n");
 
-    Directory* target_directory = static_cast<Directory*>(Get_entry(directory_name));
-    if(target_directory == nullptr){
+    Directory *target_directory = static_cast<Directory *>(Get_entry(directory_name));
+    if (target_directory == nullptr) {
         cli->Print("Target location is unreachable - null returned \r\n");
     }
 
-    if(target_directory->Type_of() != FS_entry::Type::Directory){
+    if (target_directory->Type_of() != FS_entry::Type::Directory) {
         cli->Print("Target location is not a directory \r\n");
         return -1;
     }
@@ -56,108 +56,97 @@ int Filesystem::Command_cd(vector<string> args){
     return 0;
 }
 
-string Filesystem::Command_check(vector<string> args) const{
-    if (args.size() > 2){
+string Filesystem::Command_check(vector<string> args) const {
+    if (args.size() > 2) {
         cli->Print("Invalid parameters \r\n");
         return "";
     }
     string directory_name;
-    if(args.size() == 1){
+    if (args.size() == 1) {
         directory_name = Absolute_path(".");
     } else {
         directory_name = Absolute_path(args[1]);
     }
-    if (!Entry_exists(directory_name)){
+    if (!Entry_exists(directory_name)) {
         cli->Print("Target location does not exists \r\n");
         return "";
     }
     return directory_name;
 }
 
-string Filesystem::Absolute_path(string filename) const{
-    if (filename == "."){
+string Filesystem::Absolute_path(string filename) const {
+    if (filename == ".") {
         return actual_position->Path();
     }
 
-    if (filename == "/"){
+    if (filename == "/") {
         return "/";
     }
 
-    if (filename[0] != '/'){ // Relative path, add absolute prefix
+    if (filename[0] != '/') {                     // Relative path, add absolute prefix
         filename = actual_position->Path() + filename;
     }
-    if (filename[filename.length()-1] == '/'){ // Remove / at end of string
-        filename.erase(filename.length()-1);
+    if (filename[filename.length() - 1] == '/') { // Remove / at end of string
+        filename.erase(filename.length() - 1);
     }
     return filename;
 }
 
-vector<string> Filesystem::Create_entry_path(string filename) const{
+vector<string> Filesystem::Create_entry_path(string filename) const {
     vector<string> path;
     filename = Absolute_path(filename);
     path.emplace_back("/");
-    filename.erase(0,1);
+    filename.erase(0, 1);
     unsigned int position = filename.find("/");
-    while (position != string::npos){
+    while (position != string::npos) {
         path.emplace_back(filename.substr(0, position));
-        filename.erase(0, position+1);
-        if (filename.length() > 0){
+        filename.erase(0, position + 1);
+        if (filename.length() > 0) {
             position = filename.find("/");
         } else {
             break;
         }
     }
-    if (filename.length() > 0){
+    if (filename.length() > 0) {
         path.emplace_back(filename);
     }
     return path;
 }
 
-bool Filesystem::Entry_exists(string filename) const{
+bool Filesystem::Entry_exists(string filename) const {
     return Entry_exists(Create_entry_path(filename));
 }
 
-bool Filesystem::Entry_exists(vector<string> path) const{
-
-    if (path.front() == "/" and root != nullptr and path.size() == 1){
-        // Root handling
+bool Filesystem::Entry_exists(vector<string> path) const {
+    FS_entry *target = Get_entry(path);
+    if (target != nullptr) {
         return true;
-    }
-    path.erase(path.begin());
-    Directory* actual = root;
-    for(auto entry:path){
-        if(actual->Exists(entry)){
-            //cli->Print("Actual: " + actual->Name() + " Path.back(): " + path.back() +" \r\n");
-            if(/*actual->Name() == path.back() && */entry == path.back()){ // File found
-                return true;
-            } else if(actual->Get_entry(entry)->Type_of() == FS_entry::Type::Directory){
-                // Continue deepper
-                actual = static_cast<Directory *>(actual->Get_entry(entry));
-            }
-        }
     }
     return false;
 }
 
-FS_entry* Filesystem::Get_entry(string filename) const{
+FS_entry * Filesystem::Get_entry(string filename) const {
     return Get_entry(Create_entry_path(filename));
 }
 
-FS_entry* Filesystem::Get_entry(vector<string> path) const{
-    if (path.front() == "/" and root != nullptr and path.size() == 1){
+FS_entry * Filesystem::Get_entry(vector<string> path) const {
+    if (path.front() == "/" and root != nullptr and path.size() == 1) {
         // Root handling
         return root;
     }
-    path.erase(path.begin());
-    Directory* actual = root;
-    for(auto entry:path){
-        if(actual->Exists(entry)){
-            if(/*actual->Name() == path.back() && */entry == path.back()){
+    path.erase(path.begin()); // Remove /
+    Directory *actual = root;
+    for (uint i = 0; i < path.size(); i++) {
+        if (actual->Exists(path[i])) {
+            if ((i == (path.size() - 1)) && (path[i] == path.back())) {
                 // Entry found
-                return actual->Get_entry(entry);
-            } else if(actual->Get_entry(entry)->Type_of() == FS_entry::Type::Directory){
+                return actual->Get_entry(path[i]);
+            } else if (actual->Get_entry(path[i])->Type_of() == FS_entry::Type::Directory) {
                 // Continue deepper
-                actual = static_cast<Directory *>(actual->Get_entry(entry));
+                actual = static_cast<Directory *>(actual->Get_entry(path[i]));
+            } else {
+                // Cannot go further
+                return nullptr;
             }
         }
     }
@@ -166,24 +155,27 @@ FS_entry* Filesystem::Get_entry(vector<string> path) const{
 
 int Filesystem::Make_directory(string name){
     name = Absolute_path(name);
-    if (Entry_exists(name)){
+    if (Entry_exists(name)) {
         cli->Print("Directory already exists\r\n");
         return -1;
     }
-    unsigned int position = name.find_last_of("/");
+    unsigned int position        = name.find_last_of("/");
     string parent_directory_name = name.substr(0, position);
-    if (parent_directory_name == ""){
+    if (parent_directory_name == "") {
         parent_directory_name = "/";
     }
 
-    string directory_name = name.substr(position+1);
+    string directory_name = name.substr(position + 1);
 
-    Directory* parent_directory = static_cast<Directory*>(Get_entry(parent_directory_name));
-    if(parent_directory == nullptr){
+    Directory *parent_directory = static_cast<Directory *>(Get_entry(parent_directory_name));
+    if (parent_directory == nullptr) {
         cli->Print("Cannot find parent \r\n");
         return -1;
     }
-    parent_directory->Add_entry(new Directory(directory_name));
+
+    Directory *new_directory = new Directory(directory_name);
+
+    parent_directory->Add_entry(new_directory);
 
     return 0;
 }
