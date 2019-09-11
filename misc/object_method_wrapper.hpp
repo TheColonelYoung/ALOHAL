33236d8e0aec460abe229bc948b084c0a0f4b0d1
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-11 16:14:10 +0200

    Object_method_wrapper: add template specialisation for void args

--- misc/object_method_wrapper.hpp
+++ misc/object_method_wrapper.hpp
@@ -8,6 +8,10 @@
 
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 /**
  * @brief   This wrapper hold pointer to object and method. Method must be assigned to class from which is object.
  *          Also can be saved only function.
@@ -33,13 +37,11 @@ private:
      * @brief pointer to method of class, which will be invocated on saved object
      */
     return_T (class_T::*method_pointer)(args_T args) = nullptr;
-    return_T (class_T::*method_pointer_void)(void) = nullptr;
 
     /**
      * @brief Pointer to function which will be invocated
      */
     return_T (*function)(args_T args) = nullptr;
-    return_T (*function_void)(void) = nullptr;
 
 public:
     Object_method_wrapper() = default;
@@ -54,19 +56,6 @@ public:
         object_ptr(object), method_pointer(method)
     { }
 
-
-    /**
-     * @brief   Construct a new Object_method_wrapper object which holds object and his class method
-     *          This variant is used when method receive void as parametr
-     *
-     * @param object    Pointer to object which will be stored
-     * @param method    Cointer to class method which can bee Invoked on object
-     */
-    Object_method_wrapper(class_T *object, return_T(class_T::*method)(void)) :
-        object_ptr(object), method_pointer_void(method)
-    { }
-
-
     /**
      * @brief   Construct a new Object_method_wrapper object which holds pointer to function
      *
@@ -74,16 +63,6 @@ public:
      */
     Object_method_wrapper(return_T(*function)(args_T args)) :
         function(function)
-    { }
-
-    /**
-     * @brief   Construct a new Object_method_wrapper object which holds pointer to function
-     *          This variant is used when method receive void as parametr
-     *
-     * @param function  Function to hold inside wrapper
-     */
-    Object_method_wrapper(return_T(*function)(void)) :
-        function_void(function)
     { }
 
     /**
@@ -100,6 +79,58 @@ public:
         }
         return return_T();
     }
+};
+
+/**
+ * @brief   Specialized version of class which is used when method receive void as argument
+ *
+ * @tparam class_T   Type of class which is stored inside this wrapper
+ * @tparam return_T  Type which will be returned from wrapped method or function
+ */
+template <typename class_T, typename return_T>
+class Object_method_wrapper<class_T, return_T, void>
+{
+private:
+
+    /**
+     * @brief Pointer to object on which will be method invocated
+     */
+    class_T *object_ptr = nullptr;
+
+    /**
+     * @brief pointer to method of class, which will be invocated on saved object
+     */
+    return_T (class_T::*method_pointer)(void) = nullptr;
+
+    /**
+     * @brief Pointer to function which will be invocated
+     */
+    return_T (*function)(void) = nullptr;
+
+public:
+    Object_method_wrapper() = default;
+
+
+    /**
+     * @brief   Construct a new Object_method_wrapper object which holds object and his class method
+     *          This variant is used when method receive void as parametr
+     *
+     * @param object    Pointer to object which will be stored
+     * @param method    Cointer to class method which can bee Invoked on object
+     */
+    Object_method_wrapper(class_T *object, return_T(class_T::*method)(void)) :
+        object_ptr(object), method_pointer(method)
+    { }
+
+    /**
+     * @brief   Construct a new Object_method_wrapper object which holds pointer to function
+     *          This variant is used when method receive void as parametr
+     *
+     * @param function  Function to hold inside wrapper
+     */
+    Object_method_wrapper(return_T(*function)(void)) :
+        function(function)
+    { }
 
     /**
      * @brief   Invoke method on saved object or call saved function
@@ -109,9 +140,9 @@ public:
      */
     return_T Invoke() const {
         if (function) {
-            return (*(function_void))();
+            return (*(function))();
         } else if (object_ptr) {
-            return (*object_ptr.*method_pointer_void)();
+            return (*object_ptr.*method_pointer)();
         }
         return return_T();
     }
