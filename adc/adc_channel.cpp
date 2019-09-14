cd98ecde0faf798b980b82bd14d11e4ef49c1f67
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-14 23:41:57 +0200

    adc_channel: class which wrap adc and his channel as analog input

---
+++ adc/adc_channel.cpp
@@ -0,0 +1,7 @@
+#include "adc_channel.hpp"
+
+ADC_channel::ADC_channel(AD_C *ADC, int channel):
+    ADC(ADC), channel(channel)
+{
+
+}
---
+++ adc/adc_channel.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file adc_channel.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 14.09.2019
+ */
+
+#pragma once
+
+#include "adc.hpp"
+
+class ADC_channel
+{
+private:
+    AD_C *ADC;
+
+    int channel;
+
+public:
+    ADC_channel() =default;
+    ADC_channel(AD_C *ADC, int channel);
+
+    /**
+     * @brief Perform measurement on given channel
+     *
+     * @return double
+     */
+    double Measure_poll();
+
+};
