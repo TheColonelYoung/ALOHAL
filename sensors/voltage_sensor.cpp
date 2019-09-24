807a3addc3206747b81d02473fc6e49231940cc7
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-24 12:50:00 +0200

    voltage_sensor: fixed ADC channel variable name

--- sensors/voltage_sensor.cpp
+++ sensors/voltage_sensor.cpp
@@ -1,7 +1,7 @@
 #include "voltage_sensor.hpp"
 
 Voltage_sensor::Voltage_sensor(ADC_channel *ADC, double resistor_R1, double resistor_R2) :
-    Sensor("Voltage_sensor"), ADC(ADC), resistor_R1(resistor_R1), resistor_R2(resistor_R2)
+    Sensor("Voltage_sensor"), ADC_ch(ADC), resistor_R1(resistor_R1), resistor_R2(resistor_R2)
 {
     Register_quantity("voltage", this, &Voltage_sensor::Voltage, true);
     if (resistor_R1 != 0 and resistor_R2 != 0){
