003b8f56a0f55c347c8cece067585f7bf0e117be
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-24 12:49:47 +0200

    display: class for generic display

---
+++ display/display.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file display.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 25.09.2019
+ */
+
+#pragma once
+
+class Display{
+private:
+
+public:
+
+    virtual void On() = 0;
+    virtual void Off() = 0;
+};
