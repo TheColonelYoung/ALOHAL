d90282c48545e5a095538d55ea3b27c07260062e
Petr Malanik <malanik.petr.gm@gmail.com>
2019-09-26 15:28:05 +0200

    SSD1306: add Initialization method and documentation

--- display/SSD1306.cpp
+++ display/SSD1306.cpp
@@ -1,14 +1,29 @@
 #include "SSD1306.hpp"
 
+SSD1306::SSD1306(uint res_x, uint res_y, I2C_master master, unsigned char address):
+    I2C_device(master, address), Graphical_display(res_x, res_y)
+{}
 
+void SSD1306::Init(){
+    Off();
+
+    Transmit(vector<uint8_t>{0x00, 0xd3, 0x00}); //Set display offset
+
+    Send_command(0x40); //Set Display Start Line
+
+    Send_command(0xa4); // Pixels reflect RAM
+    Send_command(0xa6); // Non-inverted display mode
+
+    Set_contrast(127);
+
+}
 
 void SSD1306::On(){
-    Transmit(vector<uint8_t>{0x00,0xaf});
-    Transmit(vector<uint8_t>{0x00,0xa5});
-    //Transmit(vector<uint8_t>{0x00,0xa7});
+    Send_command(0xaf);
 }
 
 void SSD1306::Off(){
+    Send_command(0xae);
 }
 
 int SSD1306::Put(uint x, uint y){
@@ -16,4 +31,16 @@ int SSD1306::Put(uint x, uint y){
 }
 
 int SSD1306::Clear(uint x, uint y){
+}
+
+void SSD1306::Send_command(uint8_t cmd){
+    Transmit(vector<uint8_t>{0x00, cmd});
+}
+
+void SSD1306::All_on(){
+    Send_command(0xa5);
+}
+
+int SSD1306::Set_contrast(uint8_t contrast){
+    return Transmit(vector<uint8_t>{0x00, 0x81, contrast});
 }
--- display/SSD1306.hpp
+++ display/SSD1306.hpp
@@ -10,18 +10,60 @@
 #include "graphical_display.hpp"
 #include "i2c/i2c_device.hpp"
 
-class SSD1306: public Graphical_display, public I2C_device{
-private:
+typedef unsigned int uint;
 
+/**
+ * @brief   OLED display with I2C capable controller
+ */
+class SSD1306: public Graphical_display, public I2C_device{
 public:
+    /**
+     * @brief Constructor inherited from I2C device
+     */
     using I2C_device::I2C_device;
 
+    SSD1306(uint res_x, uint res_y, I2C_master master, unsigned char address);
+
+    /**
+     * @brief Enable display via command of display
+     */
     virtual void On() final override;
 
+    /**
+     * @brief Disable display via command of display
+     */
     virtual void Off() final override;
+
+    /**
+     * @brief   Perform initialization with setting up oscillator, multiplexer,
+     *              Segment remaping, pre-charge period, disable inverting display, set charge pump
+     */
+    virtual void Init() final override;
 
     virtual int Put(uint x, uint y) final override;
 
     virtual int Clear(uint x, uint y) final override;
+
+    /**
+     * @brief Set contract of display via available command
+     *
+     * @param contrast  Constrast level in range 0-255
+     * @return int      Error code
+     */
+    virtual int Set_contrast(uint8_t contrast) final override;
+
+    /**
+     * @brief Set all pixels of display to On state
+     */
+    void All_on();
+
+private:
+    /**
+     * @brief   Transmit command to OLED constroller, before command self is transmitted empty byte,
+     *              which marks another byte as command byte
+     *
+     * @param cmd   Command which will be transmitted to OLED controller
+     */
+    void Send_command(uint8_t cmd);
 
 };
