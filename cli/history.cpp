d1de4b00fd80fccd46e50cc40aaaebdf353533a4
Petr Malanik <malanik.petr.gm@gmail.com>
2020-04-21 11:05:04 +0200

    history: add history storing and browsing

---
+++ cli/history.cpp
@@ -0,0 +1,53 @@
+#include "history.hpp"
+
+CLI_history::CLI_history(uint size) :
+    size(size){
+    cmd_history.resize(size, "");
+}
+
+string CLI_history::Up(){
+    if (pointer == end){ // End of history
+        return cmd_history[end];
+    } else {
+        string command = cmd_history[pointer];
+        pointer = (pointer - 1);
+        if (pointer == -1){
+            pointer = 4;
+        } else {
+            pointer = pointer % size;
+        }
+        return command;
+    }
+}
+
+string CLI_history::Down(){
+    if (pointer == begin){ // Begining of history
+        return "";
+    } else {
+        pointer = (pointer + 1) % size;
+        return cmd_history[pointer];
+    }
+}
+
+int CLI_history::Update(string new_command){
+    // Initialization
+    if (cmd_history[begin] == ""){
+        cmd_history[begin] = new_command;
+    } else {
+        // Skip update if last command is same as new_command
+        if (new_command == cmd_history[begin]){
+            return 1;
+        }
+        begin = (begin + 1) % size;
+        if (begin == end) {
+            end = (begin + 1) % size;
+        }
+        pointer = begin;
+        cmd_history[begin] = new_command;
+    }
+    return 0;
+}
+
+void CLI_history::Reset_pointer(){
+    pointer = begin;
+}
