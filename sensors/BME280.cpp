caafe64c77075b1513555e84e98798ddd4059093
Petr Malanik <malanik.petr.gm@gmail.com>
2020-11-01 12:14:48 +0100

    BME280: add implementation for temperature, humidity, pressure sensor BME280

---
+++ sensors/BME280.cpp
@@ -0,0 +1,120 @@
+#include "BME280.hpp"
+
+BME280::BME280(I2C_master master, unsigned char address):
+    I2C_device(master, address),
+    Sensor("BME280")
+{
+    // Allocate space in vectors for calibration data
+    temperature_calibration.resize(3);
+    humidity_calibration.resize(6);
+    pressure_calibration.resize(9);
+
+    Load_calibration_data();
+}
+
+uint8_t BME280::ID(){
+    return Read({static_cast<uint8_t>(Register::ChipID)}, 1)[0];
+}
+
+void BME280::Reset(){
+    Write({static_cast<uint8_t>(Register::Reset)},{0xb6});
+}
+
+bool BME280::Reading_calibration(){
+  uint8_t const status_reg = Read({static_cast<uint8_t>(Register::Status)}, 1)[0];
+  return status_reg & (0x01);
+}
+
+bool BME280::Measuring(){
+  uint8_t const status_reg = Read({static_cast<uint8_t>(Register::Status)}, 1)[0];
+  return status_reg & (0x08);
+}
+
+std::uint32_t BME280::Load_ADC(Register ADC_register){
+    auto ADC_value = Read({static_cast<uint8_t>(ADC_register)}, 3);
+    return (((ADC_value[0] << 16) | ADC_value[1] << 8) | ADC_value[0]) >> 4;
+}
+
+double BME280::Temperature(){
+    int32_t var1, var2, t_fine;
+
+    int32_t adc_T = Load_ADC(Register::Temperature_data);
+
+    var1 =  ((((adc_T >> 3) - ((int32_t)temperature_calibration[0] << 1))) *
+                ((int32_t)temperature_calibration[1])) >> 11;
+
+    var2 =  (((((adc_T >> 4) - ((int32_t)temperature_calibration[0])) *
+                ((adc_T >> 4) - ((int32_t)temperature_calibration[0]))) >> 12) *
+                ((int32_t)temperature_calibration[2])) >> 14;
+
+    t_fine = var1 + var2;
+
+    float T = (t_fine * 5 + 128) >> 8;
+    return T / 100;
+}
+
+void BME280::Enter_normal_mode(){
+    Enter_mode(Modes::Normal);
+}
+
+void BME280::Enter_idle_mode(){
+    Enter_mode(Modes::Idle);
+}
+
+void BME280::Force_measurement(){
+    Enter_mode(Modes::Forced);
+}
+
+void BME280::Enter_mode(Modes mode){
+    uint8_t reg_value = Read({static_cast<uint8_t>(Register::ControlMeas)},1)[0];
+    reg_value &= 0b11111100;
+    reg_value |= static_cast<uint8_t>(mode);
+    Write({static_cast<uint8_t>(Register::ControlMeas)},{reg_value});
+}
+
+bool BME280::Load_calibration_data(){
+    if(calibration_data_loaded){
+        return true;
+    }
+
+    if(Reading_calibration()){
+        Log_line(Log_levels::Error, "Loading of calibration data to registers is still in progress");
+        return false;
+    }
+
+    // Load temperature calibration: 3x uint16_t
+    uint8_t data_address = static_cast<uint8_t>(Register::DIG_T1);
+    for (uint8_t data_index = 0; data_index < 3; data_index++){
+        auto calibration_value = Read({static_cast<uint8_t>(data_address + data_index*2)}, 2);
+        temperature_calibration[data_index] = (calibration_value[1] << 8) | calibration_value[0];
+    }
+
+    // Load pressure calibration: 8x uint16_t
+    data_address = static_cast<uint8_t>(Register::DIG_P1);
+    for (uint8_t data_index = 0; data_index < 9; data_index++){
+        auto calibration_value = Read({static_cast<uint8_t>(data_address + data_index*2)}, 2);
+        pressure_calibration[data_index] = (calibration_value[1] << 8) | calibration_value[0];
+    }
+
+    // Load Humidity calibration: 5x uint16_t, but this time is not that simple as above
+    humidity_calibration[0] = Read({static_cast<uint8_t>(Register::DIG_H1)},1)[0];
+
+    auto calibration_value = Read({static_cast<uint8_t>(Register::DIG_H2)}, 2);
+    humidity_calibration[1] = (calibration_value[1] << 8) | calibration_value[0];
+
+    humidity_calibration[2] = Read({static_cast<uint8_t>(Register::DIG_H3)},1)[0];
+
+    calibration_value = Read({static_cast<uint8_t>(Register::DIG_H4)}, 2);
+    humidity_calibration[3] =  (     calibration_value[0] << 4)
+                                |   (calibration_value[1] & 0x0f) | 0xf0;
+
+    calibration_value = Read({static_cast<uint8_t>(Register::DIG_H5)}, 2);
+    humidity_calibration[4] =  (     calibration_value[0] & 0x0f)
+                                |   (calibration_value[1] << 4 );
+
+    humidity_calibration[5] = Read({static_cast<uint8_t>(Register::DIG_H6)},1)[0];
+
+    calibration_data_loaded = true;
+
+    return true;
+}
---
+++ sensors/BME280.hpp
@@ -0,0 +1,163 @@
+/**
+ * @file BME280.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 28.10.2020
+ */
+
+#pragma once
+
+#include "i2c/i2c_device.hpp"
+#include "sensor.hpp"
+#include "modifiers/loggable.hpp"
+
+using namespace std;
+
+/**
+ * @brief
+ *
+ *
+ *          In non-volatile memory contains read-only calibration data
+ */
+class BME280 : public I2C_device, private Sensor, public Loggable
+{
+private:
+    /**
+     * @brief   Address of used registers from BME280
+     */
+    enum class Register: uint8_t {
+        DIG_T1           = 0x88,
+
+        DIG_P1           = 0x8e,
+
+        DIG_H1           = 0xa1,
+        DIG_H2           = 0xe1,
+        DIG_H3           = 0xe3,
+        DIG_H4           = 0xe4,
+        DIG_H5           = 0xe5,
+        DIG_H6           = 0xe7,
+
+        ChipID           = 0xd0,
+        Reset            = 0xe0,
+
+        CAL26            = 0xe1,
+
+        ControlHumid     = 0xf2,
+        Status           = 0Xf3,
+        ControlMeas      = 0xf4,
+        Config           = 0xf5,
+
+        Temperature_data = 0xfa,
+        Pressure_data    = 0xf7,
+        Humidity_data    = 0xfd
+    };
+
+    /**
+     * @brief   Modes of sensor and constants to write to ControlMeas register to enable mode
+     */
+    enum class Modes: uint8_t {
+        Idle   = 0b00,
+        Forced = 0b01,
+        Normal = 0b11,
+    };
+
+    /**
+     * @brief   Defines if data in calibration vector are valid
+     */
+    bool calibration_data_loaded = false;
+
+    /**
+     * @brief   Temperature calibration data read from sensor
+     */
+    vector<std::uint16_t> temperature_calibration;
+
+    /**
+     * @brief   Presure calibration data read from sensor
+     */
+    vector<std::uint16_t> pressure_calibration;
+
+    /**
+     * @brief   Humidity calibration data read from sensor
+     */
+    vector<std::uint16_t> humidity_calibration;
+
+public:
+    BME280(I2C_master master, unsigned char address);
+
+    /**
+     * @brief   Reads ID from sensor register (0xd0)
+     *
+     * @return uint8_t  ID of sensor correct value is 0x60
+     */
+    uint8_t ID();
+
+    /**
+     * @brief   Checks if sensor is performing a measuring
+     *
+     * @return true     Measuring if performing now
+     * @return false    Sensor is not measuring
+     */
+    bool Measuring();
+
+    /**
+     * @brief   Data from non-volatile memory are copied after start of sensor to regsiters
+     *          This status is signaled by 0. bit in status register(STATUS)
+     *
+     * @return true     Data are still copied
+     * @return false    Data are ready in registers
+     */
+    bool Reading_calibration();
+
+    /**
+     * @brief   Perform soft reset of sensor
+     *          Writes value 0xb6 to reset register (0xE0), only 0xb6 will perform a reset, others are ignore
+     */
+    void Reset();
+
+    /**
+     * @brief   Reads temperature from sensor
+     *
+     * @return double   Temperature in celsius
+     */
+    double Temperature();
+
+    /**
+     * @brief   Set sensor to mode of continuous measurements
+     */
+    void Enter_normal_mode();
+
+    /**
+     * @brief   Set sensor to idle mode
+     */
+    void Enter_idle_mode();
+
+    /**
+     * @brief   Force one cycle of measuring
+     *          Measuring is not performed immediately
+     */
+    void Force_measurement();
+
+private:
+    /**
+     * @brief       Loads calibration data from sensor to mcu
+     *
+     * @return true     Data loading is done
+     * @return false    Data are loaded
+     */
+    bool Load_calibration_data();
+
+    /**
+     * @brief Enter selected mode
+     *
+     * @param mode  Mode to enter
+     */
+    void Enter_mode(Modes mode);
+
+    /**
+     * @brief Read ADC value from sensor
+     *
+     * @param ADC_register      ADC output registr to read
+     * @return std::uint32_t    ADC value
+     */
+    std::uint32_t Load_ADC(Register ADC_register);
+};
