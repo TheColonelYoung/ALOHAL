c631871471e564d904b0275c41388ce87eec0438
Petr Malanik <Malanik.Petr.gm@gmail.com>
2021-02-02 20:39:33 +0100

    TIM_channel: update timer interface, add modes similar to timers

--- timer/TIM_channel.cpp
+++ timer/TIM_channel.cpp
@@ -3,31 +3,69 @@
 #include "timer/timer.hpp"
 
 TIM_channel::TIM_channel(Timer *parent, uint32_t address)
-    : _address(address), _parent_timer(parent) { }
+    : _address(address), _parent_timer(parent){ }
 
-int TIM_channel::Set_mode(string mode){
-    return 0; // TODO Timer modes
-}
 
 int TIM_channel::PWM_Generate(float percentage){
-    if (percentage > 100) {
+    if (percentage > 100 or percentage < 0) {
         return -1;
     }
 
-    TIM_OC_InitTypeDef sConfigOC;
-    sConfigOC.Pulse =  _parent_timer->Handler()->Instance->ARR * (percentage / 100);
-    sConfigOC.OCMode     = TIM_OCMODE_PWM1;
-    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-    HAL_TIM_PWM_ConfigChannel(_parent_timer->Handler(), &sConfigOC, _address);
-
+    uint32_t pulse_length = _parent_timer->Autoreload() * (percentage / 100);
+    Pulse(pulse_length);
     return static_cast<int>(percentage);
 }
 
-void TIM_channel::PWM_Start(){
-    HAL_TIM_PWM_Start(_parent_timer->Handler(), _address);
+void TIM_channel::Mode(Modes mode){
+    this->mode = mode;
+
+    uint32_t new_mode = TIM_OCMODE_PWM1;
+    switch (mode) {
+        case Modes::PWM:
+            new_mode = TIM_OCMODE_PWM1;
+            break;
+        case Modes::Toggle:
+            new_mode = TIM_OCMODE_TOGGLE;
+            break;
+    }
+
+    TIM_OC_InitTypeDef sConfigOC;
+    sConfigOC.OCMode     = new_mode;
+
+    if (mode == Modes::PWM) {
+        HAL_TIM_PWM_ConfigChannel(_parent_timer->Handler(), &sConfigOC, _address);
+    } else if (mode == Modes::Toggle) {
+        HAL_TIM_OC_ConfigChannel(_parent_timer->Handler(), &sConfigOC, _address);
+    }
 }
 
-void TIM_channel::PWM_Stop(){
-    HAL_TIM_PWM_Stop(_parent_timer->Handler(), _address);
+void TIM_channel::Start(){
+    if (mode == Modes::PWM) {
+        HAL_TIM_PWM_Start(_parent_timer->Handler(), _address);
+    } else if (mode == Modes::Toggle) {
+        HAL_TIM_OC_Start(_parent_timer->Handler(), _address);
+    }
+}
+
+void TIM_channel::Stop(){
+    if (mode == Modes::PWM) {
+        HAL_TIM_PWM_Stop(_parent_timer->Handler(), _address);
+    } else if (mode == Modes::Toggle) {
+        HAL_TIM_OC_Stop(_parent_timer->Handler(), _address);
+    }
+}
+
+void TIM_channel::Start_DMA(uint32_t *address_from, uint32_t data_size){
+    HAL_TIM_OC_Start_DMA(_parent_timer->Handler(), _address, address_from, data_size);
+}
+
+
+void TIM_channel::Pulse(uint32_t pulse_length){
+    TIM_OC_InitTypeDef sConfigOC;
+    sConfigOC.OCMode     = pulse_length;
+    if (mode == Modes::PWM) {
+        HAL_TIM_PWM_ConfigChannel(_parent_timer->Handler(), &sConfigOC, _address);
+    } else if (mode == Modes::Toggle) {
+        HAL_TIM_OC_ConfigChannel(_parent_timer->Handler(), &sConfigOC, _address);
+    }
 }
--- timer/TIM_channel.hpp
+++ timer/TIM_channel.hpp
@@ -18,8 +18,19 @@ class Timer;
 using namespace std;
 
 class TIM_channel {
+
+public:
+    enum class Modes {
+        PWM,
+        Toggle
+    };
+
+private:
     int _index;
     uint32_t _address;
+
+    Modes mode = Modes::PWM;
+
 
 public:
     Timer *_parent_timer;
@@ -28,10 +39,27 @@ public:
 
     uint32_t  Address() const {return _address;};
 
-    int Set_mode(string mode);
+    /**
+     * @brief           Set mode of timer
+     *
+     * @param mode      New mode of timer
+     */
+    void Mode(Modes mode);
+
+    /**
+     * @brief           Return actual mode of timer
+     *
+     * @return Modes    Actual mode of timer
+     */
+    inline Modes Mode(){ return mode; };
 
     int PWM_Generate(float percentage);
-    void PWM_Start();
-    void PWM_Stop();
 
+    void Start();
+
+    void Stop();
+
+    void Start_DMA(uint32_t *address_from, uint32_t data_size);
+
+    void Pulse(uint32_t pulse_length);
 };
